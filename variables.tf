f75e7110b978be317b1e35d50373b80059813e93
venkatasaiKommu <Venkatasai.Kommu@Kroll.com>
2025-10-03 10:44:11 +0530

    added vars

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "ec2_instance_type" {
+  default = "t2.micro"
+  type = string
+}
+
+variable "ec2_root_storage_size" {
+  default=15
+  type = number
+}
+
+variable "ec2_ami_id" {
+  default = "ami-03aa99ddf5498ceb9"
+  type=string
+}
+
+variable "ec2_root_volume_type" {
+  default = "gp3"
+  type = string
+}
