aff92372c88787f6f2f1c986bc5efd26731fa164
venkatasaiKommu <Venkatasai.Kommu@Kroll.com>
2025-10-03 16:42:22 +0530

    added meta arguments

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "ec2_public_ip" {
-  value = aws_instance.instance[*].public_ip
+  value = [for inst in aws_instance.instance : inst.public_ip]
 }
 
 output "ec2_public_dns" {
-  value = aws_instance.instance[*].public_dns
+  value = [for inst in aws_instance.instance : inst.public_dns]
 }
