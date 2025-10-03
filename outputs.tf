1799bdc7e613e1eaa29a722a859df1142045f8b1
venkatasaiKommu <Venkatasai.Kommu@Kroll.com>
2025-10-03 11:55:58 +0530

    changed name of instance

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "ec2_public_ip" {
-  value = aws_instance.my_instance.public_ip
+  value = aws_instance.instance1.public_ip
 }
 
 output "ec2_public_dns" {
-  value = aws_instance.my_instance.public_dns
+  value = aws_instance.instance1.public_dns
 }
